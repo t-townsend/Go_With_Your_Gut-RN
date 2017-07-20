77ae8498b9585d601356095eda202827bdc69455
Trista Townsend <tristatownsend@gmail.com>
2017-07-19 21:39:02 -0700

    added first files

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#15a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
